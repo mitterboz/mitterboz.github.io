59d21c7da0ec9c3e0058a3350b4c3bcf9d852254
mitterboz <mitterboz@gmail.com>
2020-05-10 22:18:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>HTML</li>
+        <li>CSS(SASS)</li>
+        <li>JS(REACT)</li>
+        <li>GIT,WEBPACK</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
